c53ec74d34a1b2f21eb7b88cc43d27c45f7cddc4
Yaro <12fcv4@gmail.com>
2015-12-04 07:52:27 -0500

    remove extra files & fix tab/space issue

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,43 +5,43 @@ namespace Yaro\Socket;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
     /**
      * Bootstrap the application events.
      *
      * @return void
-     */
+     */ 
     public function boot()
     {
         //
     }
     
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['yaro_socket'] = $this->app->share(function($app) {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['yaro_socket'] = $this->app->share(function($app) {
             return new Socket();
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
